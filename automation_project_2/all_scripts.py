98e3c382da96d9c3e65abd742c38a62d5938f96b
BCazz <103322751+BCazz@users.noreply.github.com>
2022-12-21 04:57:25 -0500

    adding py scripts to folder

---
+++ automation_project_2/all_scripts.py
@@ -0,0 +1,5 @@
+
+
+from scripts import email_scrub
+from scripts import data_scrub
+from scripts import file_delete
---
+++ automation_project_2/scripts/model_update.py
@@ -0,0 +1,8 @@
+
+
+
+# https://openpyxl.readthedocs.io/en/stable/
+# https://psutil.readthedocs.io/en/latest/
+# https://docs.python.org/3/library/shutil.html
+# https://docs.xlwings.org/en/stable/api.html
+# https://www.psycopg.org/docs/extras.html
